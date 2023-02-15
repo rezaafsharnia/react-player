4c64e67bad52a44c9c35dc8b2007ae78e4dbb084
Reza Afsharnia <s.reza.afsharnia@gmail.com>
2023-02-15 12:14:25 +0330

    new

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
 export const musics = [
   {
-    name: "Darde Shirin",
-    artist: "Aron Afshar",
+    name: "Kheyli Ghashangi To",
+    artist: "Ahmad Saeedi",
     cover:
-      "https://music-fa.com/wp-content/uploads/2022/04/Aron-Afshar-Music-fa.com_.jpg",
+      "https://upmusics.com/wp-content/uploads/2022/08/Ahmad-Saeedi-Kheyli-Ghashangi-To-1.jpg",
     audio:
-      "https://ups.music-fa.com/tagdl/8e401/Aron%20Afshar%20-%20Darde%20Shirin%20(320).mp3",
+      "https://irsv.upmusics.com/AliBZ/Ahmad%20Saeedi%20%E2%80%93%20Khili%20Ghashngi%20To%20(128).mp3",
     id: uuidv4(),
     color: ["#313131"],
     active: true,
@@ -25,21 +25,42 @@ export const musics = [
   {
     name: "Bekhab Donya",
     artist: "Mehdi Jahani",
-    cover:
-      "https://upmusics.com/wp-content/uploads/2019/02/jahani.jpg",
+    cover: "https://upmusics.com/wp-content/uploads/2019/02/jahani.jpg",
     audio:
       "https://irsv.upmusics.com/Downloads/Musics/Mehdi%20Jahani%20-%20Bekhab%20Donya%20(320).mp3",
     id: uuidv4(),
     color: ["#313131"],
     active: false,
   },
+
   {
-    name: "Kheyli Ghashangi To",
-    artist: "Ahmad Saeedi",
+    name: "Gole Arezoo",
+    artist: "Ragheb",
     cover:
-      "https://upmusics.com/wp-content/uploads/2022/08/Ahmad-Saeedi-Kheyli-Ghashangi-To-1.jpg",
+      "https://upmusics.com/wp-content/uploads/2023/01/Ragheb-Gole-Sefid-Arezo.jpg",
     audio:
-      "https://irsv.upmusics.com/AliBZ/Ahmad%20Saeedi%20%E2%80%93%20Khili%20Ghashngi%20To%20(128).mp3",
+      "https://irsv.upmusics.com/AliBZ/Ragheb%20%7C%20Gole%20Sefid%20Arezoo%20(320).mp3",
+    id: uuidv4(),
+    color: ["#313131"],
+    active: false,
+  },
+  {
+    name: "Asheghaneh",
+    artist: "Farzad Farzin",
+    cover: "https://upmusics.com/wp-content/uploads/2017/08/1489521795.jpg",
+    audio:
+      "https://irsv.upmusics.com/Downloads/Musics/%20Farzad%20Farzin%20-%20Asheghaneh%20(128).mp3",
+    id: uuidv4(),
+    color: ["#313131"],
+    active: false,
+  },
+  {
+    name: "Darde Shirin",
+    artist: "Aron Afshar",
+    cover:
+      "https://music-fa.com/wp-content/uploads/2022/04/Aron-Afshar-Music-fa.com_.jpg",
+    audio:
+      "https://ups.music-fa.com/tagdl/8e401/Aron%20Afshar%20-%20Darde%20Shirin%20(320).mp3",
     id: uuidv4(),
     color: ["#313131"],
     active: false,
